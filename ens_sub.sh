11605519adb81ebb058972180d6a9b8325aefe0d
halehawk <haiyingx@ucar.edu>
2016-12-28 15:53:02 -0700

    added print set in EET.py

--- ens_sub.sh
+++ ens_sub.sh
@@ -12,4 +12,5 @@
 #BSUB -P STDD0002
 
 #mpirun.lsf python pyEnsSum.py --indir /glade/u/tdd/asap/verification/cesm1_3_beta06/intel.yellowstone.151 --esize 151  --verbose --tslice 1  --tag cesm1_3_beta06 --sumfile /glade/scratch/haiyingx/intel.151.beta06.nc --jsonfile beta06_ens_excluded_varlist.json --gmonly --mpi_enable 
-mpirun.lsf python pyEnsSum.py --indir /glade/p/work/jshollen/validation/cesm1_4_beta06/ensemble --esize 151   --tslice 0  --tag cesm1_2_2 --sumfile /glade/scratch/haiyingx/intel.151.cesm1_2_2_summary.2.nc --mpi_enable --mach yellowstone --compset B1850 --res f19_g16 --jsonfile ens_excluded_varlist.json --gmonly
+#mpirun.lsf python pyEnsSum.py --indir /glade/p/work/jshollen/validation/cesm1_4_beta06/ensemble --esize 151   --tslice 0  --tag cesm1_2_2 --sumfile /glade/scratch/haiyingx/intel.151.cesm1_2_2_summary.2.nc --mpi_enable --mach yellowstone --compset B1850 --res f19_g16 --jsonfile ens_excluded_varlist.json --gmonly
+mpirun.lsf python pyEnsSum.py --indir /glade/u/tdd/asap/verification/work/jshollen/validation/cesm1_4_beta06/ensemble --esize 151   --tslice 0  --tag cesm1_2_1 --sumfile /glade/scratch/haiyingx/intel.151.cesm1_2_1_summary.nc --mpi_enable --mach yellowstone --compset B1850 --res f19_g16 --jsonfile ens_excluded_varlist.json --gmonly
