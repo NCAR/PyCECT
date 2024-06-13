e33901af7a56f6c18e1fe1b78506c7512dca55ca
teopb <teopb92@gmail.com>
2024-06-13 14:47:57 -0400

    Jupyter notebook for the setup framework and supporting code and data.

--- new_model_setup/MPAS/mpas_430_pyEnsSumMPAS.sh
+++ new_model_setup/MPAS/mpas_430_pyEnsSumMPAS.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -l
-#PBS -A NTDD0005
+#PBS -A JOB_CODE
 #PBS -N ensSumMPAS
 #PBS -q main
 #PBS -l select=4:ncpus=4:mpiprocs=4
 #PBS -l walltime=12:00:00
 #PBS -j oe
-#PBS -M teopb@ucar.edu
+#PBS -M YOUR_EMAIL
 
 module load conda
 conda activate npl
 
 for ((i=0; i<=16; i++))
 do
-    mpiexec python pyEnsSumMPAS.py --esize 430 --indir /glade/campaign/cisl/asap/abaker/mpas/large_ens  --sumfile /glade/work/teopb/PyCECT/MPAS_no_pv_430_sums/mpas_sum_430_slice_3x$i.nc  --tslice $i --tag v7.1 --model mpas  --mach cheyenne --verbose --jsonfile /glade/work/teopb/PyCECT/mpas_ex_with_pv_vars.json
+    mpiexec python ../../pyEnsSumMPAS.py --esize 430 --indir PATH_TO_MODEL_RUNS  --sumfile WHERE_TO_PLACE_SUMMARY_FILES/MPAS_no_pv_430_sums/mpas_sum_430_slice_3x$i.nc  --tslice $i --tag v7.1 --model mpas  --mach MACHINE_USED_TO_CREATE_RUNS --verbose --jsonfile PATH_TO_EXCLUDE_FILE/mpas_ex_with_pv_vars.json
 done
